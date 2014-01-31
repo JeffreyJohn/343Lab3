f72ce9e325b888b54fb8061d2bacab1db1627477
lisapeterman <lisapeterman88@gmail.com>
2014-01-31 12:43:41 -0800

    Create GraphM.cpp

---
+++ GraphM.cpp
@@ -0,0 +1,54 @@
+#include "graphm.h"
+
+const int MAXNODES = 100;
+
+//----------------------------------------------------------------------------
+// constructor
+//
+GraphM::GraphM() {
+	size = 0;
+
+}
+
+//----------------------------------------------------------------------------
+// buildGraph
+// puts in edge costs
+void GraphM::buildGraph() {
+
+}
+
+//----------------------------------------------------------------------------
+// insertEdge
+//
+void GraphM::insertEdge(){
+
+}
+
+//----------------------------------------------------------------------------
+// removeEdge
+//
+void GraphM::removeEdge() {
+
+}
+
+//----------------------------------------------------------------------------
+//findShortestPath
+//
+int GraphM::findShortestPath() {
+
+}
+
+//----------------------------------------------------------------------------
+//displayAll
+//not general output, uses couts to demonstrate that the algorithm
+//works properly as shown
+void GraphM::displayAll(){
+
+}
+
+//----------------------------------------------------------------------------
+//display
+//to display one shortest distance with path
+void GraphM::display(){
+
+}
