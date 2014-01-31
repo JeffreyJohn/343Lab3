fab757f1955500c5526bf686b9363f5d4d6ded81
lisapeterman <lisapeterman88@gmail.com>
2014-01-31 13:00:02 -0800

    Update GraphM.cpp

--- GraphM.cpp
+++ GraphM.cpp
@@ -13,8 +13,34 @@ GraphM::GraphM() {
 //----------------------------------------------------------------------------
 // buildGraph
 // puts in edge costs
-void GraphM::buildGraph() {
+void GraphM::buildGraph(istream& infile) {
+	int fromNode, toNode;              // from and to node ends of edge
 
+	makeEmpty();                       // clear the graph of memory 
+
+	infile >> size;                    // read the number of nodes
+	if (infile.eof()) return;          // stop if no more data
+
+	string s;                          // used to read through to end of line
+	getline(infile, s);
+
+	// read graph node information
+	for (int i = 1; i <= size; i++) {
+		// read using setData of the NodeData class,
+		// something like: 
+		//    adjList[i].data.setData(infile);
+		// where adjList is the array of GraphNodes and
+		// data is the NodeData object inside of GraphNode
+	}
+
+	// read the edge data and add to the adjacency list
+	for (;;) {
+		infile >> fromNode >> toNode;
+		if (fromNode == 0 && toNode == 0) return;     // end of edge data
+
+		// insert the edge into the adjacency list for fromNode
+	}
+}
 }
 
 //----------------------------------------------------------------------------
@@ -50,5 +76,11 @@ void GraphM::displayAll(){
 //display
 //to display one shortest distance with path
 void GraphM::display(){
+
+}
+
+//----------------------------------------------------------------------------
+//makeEmpty
+void GraphM::makeEmpty(){
 
 }
